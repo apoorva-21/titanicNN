02ddcf7d2be09d7eba984b5e1227bccb8d17ef62
Apoorva Gokhale <apoorvagokhale89@gmail.com>
2019-02-10 12:42:24 +0530

    cleaned

--- nn_1hidden.py
+++ nn_1hidden.py
@@ -48,19 +48,12 @@ X = normalize(X)
 X = X.T
 y = np.reshape(y, (1, y.shape[0]))
 
-# print X.shape
-# print y.shape
-
 n_inputs = X.shape[0]
 n_HL1 = 3
-# n_HL2 = 2
 n_Out = 1
 
 W_HL1 = np.random.rand(n_HL1, n_inputs) / np.sqrt(n_inputs)
 b_HL1 = np.zeros((n_HL1, 1)) / np.sqrt(n_inputs)
-
-# W_HL2 = np.random.rand(n_HL2, n_HL1) / np.sqrt(n_HL1)
-# b_HL2 = np.zeros((n_HL2, 1)) / np.sqrt(n_HL1)
 
 W_Out = np.random.rand(n_Out, n_HL1) / np.sqrt(n_HL1)
 b_Out = np.zeros((n_Out, 1)) / np.sqrt(n_HL1)
@@ -72,9 +65,6 @@ def forward_prop(X_in):
 	z_1 = np.matmul(W_HL1, X_in) + b_HL1
 	a_1 = sigmoid(z_1)
 
-	# z_2 = np.matmul(W_HL2, a_1) + b_HL2
-	# a_2 = relu(z_2)
-
 	z_Out = np.matmul(W_Out, a_1) + b_Out
 	a_Out = sigmoid(z_Out)
 
@@ -84,30 +74,18 @@ def backward_prop(X_in, Y_true, a_1, a_Out):
 
 	dz_Out = a_Out - Y_true
 	dW_Out = np.matmul(dz_Out, a_1.T)
-	# print dW_Out
-	# print dW_Out.shape
 	db_Out = np.reshape(np.sum(dz_Out, axis = 1), (b_Out.shape[0], b_Out.shape[1]))
-
-	# da_2 = np.matmul(W_Out.T,dz_Out)
-	# dz_2 =  da_2.copy()
-	# dz_2[a_2 == 0] = 0
-	# dW_HL2 = np.matmul(dz_2, a_1.T)
-	# db_HL2 = np.reshape(np.sum(dz_2, axis = 1),(b_HL2.shape[0], b_HL2.shape[1]))
 	
 	da_1 = np.matmul(W_Out.T, dz_Out)
-	# dz_1 =  da_1.copy()
-	# dz_1[a_1 == 0] = 0
 	dz_1 = da_1 * (a_1) * (1. - a_1)
 	dW_HL1 = np.matmul(dz_1, X_in.T)
 	db_HL1 = np.reshape(np.sum(dz_1, axis = 1),(b_HL1.shape[0], b_HL1.shape[1]))
 
 	return dW_HL1, db_HL1, dW_Out, db_Out
 
-print '\n\nInitial Weights : '
+print '\n\nInitial Params : '
 print 'W_HL1 : ', W_HL1
 print 'b_HL1 : ', b_HL1
-# print 'W_HL2 : ', W_HL2
-# print 'b_HL2 : ', b_HL2
 print 'W_Out : ', W_Out
 print 'b_Out : ', b_Out
 
@@ -125,16 +103,12 @@ for i in range(N_EPOCHS):
 	#update params::
 	W_HL1 -= ALPHA * dW_HL1
 	b_HL1 -= ALPHA * db_HL1
-	# W_HL2 -= ALPHA * dW_HL2
-	# b_HL2 -= ALPHA * db_HL2
 	W_Out -= ALPHA * dW_Out
 	b_Out -= ALPHA * db_Out
 
 print '\n\nTrained Weights : '
 print 'W_HL1 : ', W_HL1
 print 'b_HL1 : ', b_HL1
-# print 'W_HL2 : ', W_HL2
-# print 'b_HL2 : ', b_HL2
 print 'W_Out : ', W_Out
 print 'b_Out : ', b_Out
 
@@ -155,14 +129,12 @@ X_test = X_test.values
 X_test = process_gender(X_test)
 X_test = normalize(X_test)
 X_test= X_test.T
-# X_test = np.hstack([np.ones((X_test.shape[0], 1)), X_test])
 
 y_test = y_test.values
 
 #inference on train set
 a_1_train, a_Out_train = forward_prop(X)
-# print a_Out_test
-# exit()
+
 correct = 0
 print 'Accuracy on the train set : '
 for i in range(X.shape[1]):
