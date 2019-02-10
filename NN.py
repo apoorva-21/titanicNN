a28b60d69bfd2e2ef23586f2658bbb74b1332d9a
Apoorva Gokhale <apoorvagokhale89@gmail.com>
2019-02-10 12:34:04 +0530

    Update NN.py

--- NN.py
+++ NN.py
@@ -6,159 +6,188 @@ import random
 
 TRAIN_FILE = './data/train.csv'
 TEST_FILE = './data/test.csv'
+TEST_LABELS_FILE = './data/gender_submission.csv'
 VALIDATION_SPLIT = 0.1
+ALPHA = 1e-3
+N_EPOCHS = 300
 
-
-def activate(Z, activation = 'relu'):
-	if activation == 'sigmoid':
-		expo = np.exp(-1 * Z)
-		return 1.0/(1 + expo)
-	elif activation == 'relu':
-		Z[Z < 0] = 0
-		return Z
-def update(parameter, derivative, learning_rate, update_rule = 'grad_descent'):
-	if update_rule == 'grad_descent':
-		return parameter - LR * derivative
+def process_gender(X):
+	genders = list(set(X.T[1]))
+	#0=male
+	for i in range(X.shape[0]):
+		X[i][1] = genders.index(X[i][1])
+	return X
 
 def normalize(matrix):
 	matrix = np.array(matrix.T, dtype= np.float32)
-	for i in range(matrix.shape[0]):
-		matrix[i] = (matrix[i] - np.average(matrix[i]))/(np.max(matrix[i]) - np.min(matrix[i]))
+	for i in range(matrix.shape[0]): #i.e. for each attribute in the dataset
+		matrix[i] = (matrix[i] - np.average(matrix[i]))/(np.max(matrix[i]) - np.min(matrix[i]) + 1)
 	return matrix.T
 
+def sigmoid(x):
+	expo = np.exp(-x)
+	z = 1. / (1. + expo)
+	return z
+
+def relu(x):
+	z = x.copy()
+	z[z < 0] = 0
+	return z
+
 df = pd.read_csv(TRAIN_FILE)
-df = df.drop(['PassengerId','Name','Cabin'], axis = 1)
-y = df.as_matrix(['Survived'])
+df = df.drop(['PassengerId','Name','Cabin', 'Ticket', 'Parch', 'Fare', 'Embarked'], axis = 1)
+df = df.dropna()
+
+y = df['Survived'].values
 df = df.drop(['Survived'], axis = 1)
+X = df.values
 
-df = df.dropna(axis= 0, how = 'any')
-
-dirtyFeatures = df.as_matrix()
-
-genders = list(set(dirtyFeatures.T[1]))
-#0=male
-for i in range(dirtyFeatures.shape[0]):
-	dirtyFeatures[i][1] = genders.index(dirtyFeatures[i][1])
-
-embarkLocs = list(set(dirtyFeatures.T[7]))
-for i in range(dirtyFeatures.shape[0]):
-	dirtyFeatures[i][7] = embarkLocs.index(dirtyFeatures[i][7])
-
-reInt = re.compile(r'^[-+]?([1-9]\d*|0)$')
-
-for i in range(dirtyFeatures.shape[0]):
-	ticketNoList = dirtyFeatures[i][5].split(' ')
-	ticketNo = ticketNoList[len(ticketNoList) - 1]
-	if(reInt.match(ticketNo)):
-		dirtyFeatures[i][5] = int(ticketNo)
-	else:
-		dirtyFeatures[i][5] = 0
-
-X = dirtyFeatures
+X = process_gender(X)
 X = normalize(X)
 
-y = np.ravel(y)
-indices = np.arange(X.shape[0])
-np.random.shuffle(indices)
-X = X[indices]
-y = y[indices]
+X = X.T
+y = np.reshape(y, (1, y.shape[0]))
 
-numVal = int(VALIDATION_SPLIT * X.shape[0])
-X_train = X[:-numVal]
-y_train = y[:-numVal]
-X_test = X[-numVal:]
-y_test = y[-numVal:]
+# print X.shape
+# print y.shape
 
-#the network:
-nAttributes = X_train.shape[1]
-nSamples = X_train.shape[0]
-inputs = X_train.T
+n_inputs = X.shape[0]
+n_HL1 = 3
+# n_HL2 = 2
+n_Out = 1
 
-nodesHL1 = 3
-nodesHL2 = 3
-nodesOUT = 1
+W_HL1 = np.random.rand(n_HL1, n_inputs) / np.sqrt(n_inputs)
+b_HL1 = np.zeros((n_HL1, 1)) / np.sqrt(n_inputs)
 
-activationHL1 = 'relu'
-activationHL2 = 'relu'
-activationOUT = 'sigmoid'
+# W_HL2 = np.random.rand(n_HL2, n_HL1) / np.sqrt(n_HL1)
+# b_HL2 = np.zeros((n_HL2, 1)) / np.sqrt(n_HL1)
 
-update_rule = 'grad_descent'
+W_Out = np.random.rand(n_Out, n_HL1) / np.sqrt(n_HL1)
+b_Out = np.zeros((n_Out, 1)) / np.sqrt(n_HL1)
 
-W_HL1 = np.random.rand(nodesHL1, nAttributes) #3x7
-b_HL1 = np.zeros((nodesHL1, 1)) #3x1
-W_HL2 = np.random.rand(nodesHL2, nodesHL1) #3x3
-b_HL2 = np.zeros((nodesHL2, 1)) #3x1
-W_OUT = np.random.rand(nodesOUT, nodesHL2) #1x3
-b_OUT = np.zeros((nodesOUT, 1)) #1x1
 
-NUM_EPOCHS = 1500
-LR = 0.01
-X_train = X_train.T
+def forward_prop(X_in):
+	global W_HL1, b_HL1,W_Out, b_Out
 
-z_HL1 = []
-a_HL1 = []
-z_HL2 = []
-a_HL2 = []
-z_OUT = []
-a_OUT = []
-for i in range(NUM_EPOCHS):
+	z_1 = np.matmul(W_HL1, X_in) + b_HL1
+	a_1 = sigmoid(z_1)
 
-	#forward propagation::
-	# print b_HL1.shape
-	z_HL1 = np.dot(W_HL1, X_train) + b_HL1 #3x7 * 7xb + 3x1 = 3xb
-	a_HL1 = activate(z_HL1, activationHL1) #3xb
+	# z_2 = np.matmul(W_HL2, a_1) + b_HL2
+	# a_2 = relu(z_2)
+
+	z_Out = np.matmul(W_Out, a_1) + b_Out
+	a_Out = sigmoid(z_Out)
+
+	return a_1, a_Out
+
+def backward_prop(X_in, Y_true, a_1, a_Out):
+
+	dz_Out = a_Out - Y_true
+	dW_Out = np.matmul(dz_Out, a_1.T)
+	# print dW_Out
+	# print dW_Out.shape
+	db_Out = np.reshape(np.sum(dz_Out, axis = 1), (b_Out.shape[0], b_Out.shape[1]))
+
+	# da_2 = np.matmul(W_Out.T,dz_Out)
+	# dz_2 =  da_2.copy()
+	# dz_2[a_2 == 0] = 0
+	# dW_HL2 = np.matmul(dz_2, a_1.T)
+	# db_HL2 = np.reshape(np.sum(dz_2, axis = 1),(b_HL2.shape[0], b_HL2.shape[1]))
 	
-	z_HL2 = np.dot(W_HL2, a_HL1) + b_HL2 #3x3 * 3xb + 3xb = 3xb
-	a_HL2 = activate(z_HL2, activationHL2) #3xb
-	
-	z_OUT = np.dot(W_OUT, a_HL2) + b_OUT #1x3 * 3xb + 1x1 = 1xb
-	a_OUT = activate(np.array(z_OUT,dtype = np.float32), activationOUT) #1xb
+	da_1 = np.matmul(W_Out.T, dz_Out)
+	# dz_1 =  da_1.copy()
+	# dz_1[a_1 == 0] = 0
+	dz_1 = da_1 * (a_1) * (1. - a_1)
+	dW_HL1 = np.matmul(dz_1, X_in.T)
+	db_HL1 = np.reshape(np.sum(dz_1, axis = 1),(b_HL1.shape[0], b_HL1.shape[1]))
 
-	#loss computation::
-	loss = np.sum(-y_train.T * np.log(a_OUT) -(1 - y_train.T) * np.log(1 - a_OUT)) / nSamples
-	print 'Epoch : {}   Loss : {}'.format(i,loss)
-	#backward propagation::
-	da_OUT = -np.sum(y_train.T - a_OUT) / nSamples#single valued
-	dz_OUT = a_OUT * (1 - a_OUT) * da_OUT #1xb
-	dW_OUT = np.dot(dz_OUT, a_HL2.T) #1xb * bx3 = 1x3
-	db_OUT = np.sum(dz_OUT)
-	
-	da_HL2 = np.dot(W_OUT.T, dz_OUT) #3x1 * 1xb = 3xb
-	drelu = np.zeros_like(da_HL2)
-	drelu[a_HL2 > 0] = 1
-	dz_HL2 = drelu * da_HL2 # 3xb * 3xb one to one
-	dW_HL2 = np.dot(dz_HL2, a_HL1.T) #3xb * bx3 = 3x3
-	db_HL2 = np.reshape(np.sum(dz_HL2, axis = 1),(3,1)) #3x1
-	
-	da_HL1 = np.dot(W_HL2.T, dz_HL2) # 3x3 * 3xb = 3xb
-	drelu = np.zeros_like(da_HL1) #3xb
-	drelu[a_HL1 > 0] = 1
-	dz_HL1 = drelu * da_HL1 #3xb * 3xb one to one
-	dW_HL1 = np.dot(dz_HL1, X_train.T)  # 3xb * bx7 = 3x7
-	db_HL1 = np.reshape(np.sum(dz_HL1, axis = 1),(3,1))
-	
-	#weight update::
-	W_HL1 = update(W_HL1, dW_HL1, LR, update_rule)
-	b_HL1 = update(b_HL1, db_HL1, LR, update_rule)
-	
-	W_HL2 = update(W_HL2, dW_HL2, LR, update_rule)
-	b_HL2 = update(b_HL2, db_HL2, LR, update_rule)
-	
-	W_OUT = update(W_OUT, dW_OUT, LR, update_rule)
-	b_OUT = update(b_OUT, db_OUT, LR, update_rule)
+	return dW_HL1, db_HL1, dW_Out, db_Out
 
-#evaluate validation accuracy:
-z_HL1 = np.dot(W_HL1, X_test.T) + b_HL1 #3x7 * 7xb + 3x1 = 3xb
-a_HL1 = activate(z_HL1, activationHL1) #3xb
-z_HL2 = np.dot(W_HL2, a_HL1) + b_HL2 #3x3 * 3xb + 3xb = 3xb
-a_HL2 = activate(z_HL2, activationHL2) #3xb
-z_OUT = np.dot(W_OUT, a_HL2) + b_OUT #1x3 * 3xb + 1x1 = 1xb
-a_OUT = activate(np.array(z_OUT,dtype = np.float32), activationOUT) #1xb
+print '\n\nInitial Weights : '
+print 'W_HL1 : ', W_HL1
+print 'b_HL1 : ', b_HL1
+# print 'W_HL2 : ', W_HL2
+# print 'b_HL2 : ', b_HL2
+print 'W_Out : ', W_Out
+print 'b_Out : ', b_Out
 
+print '\n\n'
+print '#' * 50
+for i in range(N_EPOCHS):
+	
+
+	#get activations::
+	a_1, a_Out = forward_prop(X)
+
+	#get gradients::
+	dW_HL1, db_HL1, dW_Out, db_Out = backward_prop(X, y, a_1, a_Out)
+
+	#update params::
+	W_HL1 -= ALPHA * dW_HL1
+	b_HL1 -= ALPHA * db_HL1
+	# W_HL2 -= ALPHA * dW_HL2
+	# b_HL2 -= ALPHA * db_HL2
+	W_Out -= ALPHA * dW_Out
+	b_Out -= ALPHA * db_Out
+
+print '\n\nTrained Weights : '
+print 'W_HL1 : ', W_HL1
+print 'b_HL1 : ', b_HL1
+# print 'W_HL2 : ', W_HL2
+# print 'b_HL2 : ', b_HL2
+print 'W_Out : ', W_Out
+print 'b_Out : ', b_Out
+
+X_test = pd.read_csv(TEST_FILE)
+y_test = pd.read_csv('./data/gender_submission.csv')[['Survived']]
+#remove unnecessary features::
+X_test.drop(['PassengerId','Name','Cabin', 'Ticket', 'Parch', 'Fare', 'Embarked'], axis=1, inplace=True)
+
+age = X_test[['Age']].values
+to_be_deleted = []
+for i in range(len(age)):
+	if np.isnan(age[i]):
+		to_be_deleted.append(i)
+X_test.drop(X_test.index[to_be_deleted], axis=0, inplace=True)
+y_test.drop(y_test.index[to_be_deleted], axis=0, inplace=True)
+
+X_test = X_test.values
+X_test = process_gender(X_test)
+X_test = normalize(X_test)
+X_test= X_test.T
+# X_test = np.hstack([np.ones((X_test.shape[0], 1)), X_test])
+
+y_test = y_test.values
+
+#inference on train set
+a_1_train, a_Out_train = forward_prop(X)
+# print a_Out_test
+# exit()
 correct = 0
-for i in range(numVal):
-	if a_OUT[0][i] >= 0.5 and y_test[i] == 1:
-		correct += 1.0
-	if a_OUT[0][i] < 0.5 and y_test[i] == 0:
-		correct += 1.0	
-print correct / numVal
+print 'Accuracy on the train set : '
+for i in range(X.shape[1]):
+	output = 0
+	# print a_Out_train[0,i], y[0,i]
+	if a_Out_train[0,i] > 0.5:
+		output = 1
+	if output == y[0,i]:
+		correct += 1
+
+print correct * 1. / X.shape[1]
+
+
+#inference on test set
+a_1_test, a_Out_test = forward_prop(X_test)
+# print a_Out_test
+# exit()
+correct = 0
+print 'Accuracy on the test set : '
+for i in range(X_test.shape[1]):
+	output = 0
+	if a_Out_test[0,i] > 0.5:
+		output = 1
+	if output == y_test[i]:
+		correct += 1
+	# print a_Out_test[0,i], y_test[i]
+
+print correct * 1. / X_test.shape[1]
